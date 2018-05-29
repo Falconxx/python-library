a790b06de7969ae010fb3dd6ae2309360bfe4ca8
Arkadiusz Sokołowski <user@users-MBP.home>
2018-05-30 01:21:29 +0200

    0.2 version

--- python/calc_pack/hmcalc.py
+++ python/calc_pack/hmcalc.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
 def tiles(width, height, tWidth, tHeight):
-	area = width * height
-	tile = tWidth * tHeight
+	area = width * height * 10
+	tile = tWidth * tHeight *2
 	return area / tile
 
 	
 def paint(width, height, performance):
-	area = width * height
+	area = width * height * 3
 	return area / performance
 	
 def panels(width, height, tWidth, tHeight):
-	area = width * height;
-	panel = tWidth * tHeight;
+	area = width * height * 5;
+	panel = tWidth * tHeight * 4;
 	return area / panel

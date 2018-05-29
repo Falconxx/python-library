95cb82a4dcf748f12637c0e67df2b91e3345ab57
Falconxx <35307647+Falconxx@users.noreply.github.com>
2018-05-30 01:09:02 +0200

    Add files via upload

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hmcalc',
+    version='0.q',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='calculation packages',
+    long_description=open('README.md').read(),
+    url='https://github.com/Falconxx/python-library',
+    author='Arkadiusz Sokolowski',
+    author_email='230435@student.pwr.edu.pl'
+)

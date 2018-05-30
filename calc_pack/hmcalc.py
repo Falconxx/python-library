804b225f7569c0ec467866e654976a15b6dfc06e
Arkadiusz Sokołowski <user@users-MBP.home>
2018-05-30 02:54:41 +0200

    v1.0

--- calc_pack/hmcalc.py
+++ calc_pack/hmcalc.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python2.7
 
 def tiles(width, height, tWidth, tHeight):
-	area = width * height * 5
-	tile = tWidth * tHeight * 3
+	area = width * height * 7
+	tile = tWidth * tHeight * 5
 	return area / tile
 
 	
 def paint(width, height, performance):
-	area = width * height * 2
+	area = width * height * 4
 	return area / performance
 	
 def panels(width, height, tWidth, tHeight):
-	area = width * height * 3;
-	panel = tWidth * tHeight * 5;
+	area = width * height * 5;
+	panel = tWidth * tHeight * 10;
 	return area / panel
--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 
 import calc_pack
 import dirlist
 
 print('APLIKACJA KORZYSTA Z PAKIETÓW calc_pack ORAZ listdir')
 
-print('\nObliczamy ile płytek 10x20 potrzeba, aby wyłożyć powierzchnię 120x340:')
-print(calc_pack.hmcalc.tiles(120,340,10,20))
+print('\nObliczamy cene przewozu ladunku o wielkości 150mx400m w ciągu min 5 dni max 10 dni:')
+print(calc_pack.hmcalc.tiles(120,340,5,10))
 
 print('\nListujemy analizę obecnego katalogu (linki symboliczne, linki wskazujące na pliki)')
 print(dirlist.dirlist.lll())
